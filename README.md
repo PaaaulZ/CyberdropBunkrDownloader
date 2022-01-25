1e839b564dc59cc1e345c5aac15a16b7e068a1e8
PaaaulZ <46759927+PaaaulZ@users.noreply.github.com>
2022-01-25 21:24:14 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # CyberdropDownloader
 Simple downloader for cyberdrop.me
+
+
+# Usage
+
+Before using install requirements with
+
+```
+pip3 install -r requirements.txt
+```
+
+```
+python3 dump.py -u [url]
+```
+
+Download only specific extensions (optional):
+
+```
+python3 dump.py -u [url] -e [extensions (comma separated)] ex: python3 dump.py -u [url] -e [.jpg,.mp4]
+```
---
+++ dump.py
@@ -0,0 +1,66 @@
+from distutils import extension
+import requests
+from bs4 import BeautifulSoup
+import argparse
+import sys
+import os
+from os.path import splitext
+from urllib.parse import urlparse 
+
+
+def get_items_list(url, extensions):
+
+    extensions_list = []
+    if extensions is not None and extensions != "":
+        extensions_list = extensions.split(',')
+
+    r = requests.get(url)
+    if r.status_code != 200:
+        raise Exception(f"HTTP error {r.status_code}")
+
+    soup = BeautifulSoup(r.content, 'lxml')
+    items = soup.find_all('a', {'class': 'image'})
+    for item in items:
+        item_url = item['href']
+        extension = get_extension_from_url(item_url)
+        if extension in extensions_list or len(extensions_list) == 0:
+            print(f"[+] Downloading {item_url}")
+            download(item_url)
+
+
+def download(item_url):
+
+    if not os.path.isdir('cyberdrop_downloads'):
+        os.mkdir('cyberdrop_downloads')
+    
+    file_name = get_file_name_from_url(item_url)
+    with open(os.path.join('cyberdrop_downloads', file_name), 'wb') as f:
+        r = requests.get(item_url)
+        f.write(r.content)
+
+    return
+
+def get_extension_from_url(url):
+    path = urlparse(url).path
+    ext = splitext(path)[1]
+    return ext
+
+def get_file_name_from_url(url):
+    path = urlparse(url).path
+    file_name = os.path.basename(path)
+    return file_name
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(sys.argv[1:])
+    parser.add_argument("-u", help="Url to fetch", type=str)
+    parser.add_argument("-e", help="Extensions to download (comma separated)", type=str)
+
+    args = parser.parse_args()
+
+    if args.u is None or args.u == "":
+        raise Exception("No url specified")
+
+    get_items_list(args.u, args.e)
+    
+    
