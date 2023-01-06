f50973934c5a6096c501c0f4e299fb455c953cd5
ChdML1 <82635656+ChdML1@users.noreply.github.com>
2023-01-06 21:57:51 +0300

    Added check_bunkr_status and fixed some bugs

--- dump.py
+++ dump.py
@@ -5,9 +5,7 @@ import sys
 import os
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse 
-
-
-def get_items_list(url, extensions, min_file_size, use_album_id, only_export, custom_path=None):
+def get_items_list(url, extensions, min_file_size, use_album_id, only_export, custom_path=None, ignore_error=False):
 
     extensions_list = extensions.split(',') if extensions is not None else []
     hostname = get_url_data(url)['hostname']
@@ -17,11 +15,18 @@ def get_items_list(url, extensions, min_file_size, use_album_id, only_export, cu
         raise Exception(f"HTTP error {r.status_code}")
 
     soup = BeautifulSoup(r.content, 'html.parser')
-    if hostname in ['bunkr.is', 'stream.bunkr.is', 'bunkr.ru']:
+    if hostname in ['bunkr.is', 'stream.bunkr.is', 'bunkr.ru', 'stream.bunkr.ru']:
+        check_bunkr_status(ignore_error);
         item_urls = []
         album_or_file = 'file' if hostname in ['stream.bunkr.is', 'stream.bunkr.ru'] else 'album'
         json_data_element = soup.find("script", {"id": "__NEXT_DATA__"})
         json_data = json.loads(json_data_element.string)
+        if len(json_data['props']['pageProps']) == 0:
+            print("[-] Failed to load the album/file, maybe try visiting the album in a browser?")
+            # Sometimes when loading album, bunkr will return a page 
+            # saying "Loading...", opening the album/file
+            # seems to fix that 
+            return
         files = json_data['props']['pageProps']['album']['files'] if album_or_file == 'album' else [json_data['props']['pageProps']['file']]
         if album_or_file == 'file':
             item_urls = [f"{file['mediafiles']}/{file['name']}" for file in files if int(file['size']) > (min_file_size * 1000)]
@@ -40,6 +45,7 @@ def get_items_list(url, extensions, min_file_size, use_album_id, only_export, cu
             extension = get_url_data(item_url)['extension']
             if ((extension in extensions_list or len(extensions_list) == 0) and (item_url not in already_downloaded_url)):
                 print(f"[+] Downloading {item_url}")
+                
                 download(item_url, download_path, hostname in ['bunkr.ru', 'bunkr.is'])
     else:
         export_list(item_urls, download_path)
@@ -49,6 +55,9 @@ def download(item_url, download_path, is_bunkr=False):
 
     file_name = get_url_data(item_url)['file_name']
     with requests.get(item_url, headers={'Referer': 'https://stream.bunkr.ru/', 'User-Agent': 'Mozila/5.0'} if is_bunkr else {}, stream=True) as r:
+        if r.url in ["https://static.bunkr.ru/v/maintenance.mp4", "https://static.bunkr.is/v/maintenance.mp4"]:
+            print(f"[-] Error Downloading \"{file_name}\", server is under maintenance!")
+            return
         with open(os.path.join(download_path, file_name), 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
@@ -106,9 +115,29 @@ def mark_as_downloaded(item_url, download_path):
 
     return
 
-
+def check_bunkr_status(ignore=False):
+    print("Checking bunkr statuses...")
+    r = requests.get("https://status.bunkr.ru/")
+    if r.status_code != 200:
+        raise Exception(f"HTTP error {r.status_code}")
+    soup = BeautifulSoup(r.content, 'html.parser')
+    els = soup.find_all("div", {"class": "column"})
+    for el in els:
+        childrenList = list(el.children)
+        server = childrenList[1].text
+        status = childrenList[3].text
+        print(f"{server}: {status}")
+        if status not in ["Operational", "Degraded performance"]:
+            print("\n")
+            print(f"{server} Seems to not be operational! videos that are downloaded from that server will be broken", end="", flush=True)
+            if ignore == False:
+                print(", if you want to ignore this error run the script again with the argument \"-ignore\"")
+                exit(1)
+            else:
+                print("\n")
+    return    
+    
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(sys.argv[1:])
     parser.add_argument("-u", help="Url to fetch", type=str, required=True)
     parser.add_argument("-e", help="Extensions to download (comma separated)", type=str)
@@ -116,7 +145,7 @@ if __name__ == '__main__':
     parser.add_argument("-i", help="Use album id instead of album name for the folder name (only for Bunkr)", action="store_true")
     parser.add_argument("-p", help="Path to custom downloads folder")
     parser.add_argument("-w", help="Export url list (ex: for wget)", action="store_true")
+    parser.add_argument("-ignore", help="Ignores if servers were down",action="store_true")
 
     args = parser.parse_args()
-
-    get_items_list(args.u, args.e, args.s, args.i, args.w, args.p)
+    get_items_list(args.u, args.e, args.s, args.i, args.w, args.p, args.ignore)
